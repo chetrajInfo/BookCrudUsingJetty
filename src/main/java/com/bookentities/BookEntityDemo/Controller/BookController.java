befd38a9b2f6bfd7e84150a49db49a25362b9226
myinfo <chetraj0101@gmail.com>
2023-09-28 01:13:22 -0500

    added the Book controller class

---
+++ src/main/java/com/bookentities/BookEntityDemo/Controller/BookController.java
@@ -0,0 +1,38 @@
+package com.bookentities.BookEntityDemo.Controller;
+
+import com.bookentities.BookEntityDemo.Model.Book;
+import com.bookentities.BookEntityDemo.Service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Book saveBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+}
