73527a096587217c8179cb02c628ea67975d95a9
myinfo <chetraj0101@gmail.com>
2023-09-28 00:54:51 -0500

    added Book class in Model and BookRepo in Repository

---
+++ src/main/java/com/bookentities/BookEntityDemo/Model/Book.java
@@ -0,0 +1,19 @@
+package com.bookentities.BookEntityDemo.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+
+}
---
+++ src/main/java/com/bookentities/BookEntityDemo/Repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.bookentities.BookEntityDemo.Repository;
+
+import com.bookentities.BookEntityDemo.Model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository {
+
+    List<Book> findAll();
+
+    Optional<Book> findById(Long id);
+
+    Book save(Book book);
+
+    void deleteById(Long id);
+}
