52394bb9397f8b9512054fe1f6a0375bc678b3ab
myinfo <chetraj0101@gmail.com>
2023-09-28 00:48:30 -0500

    adding new jetty test file

---
+++ src/main/java/com/bookentities/BookEntityDemo/BookEntityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bookentities.BookEntityDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEntityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEntityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookentities/BookEntityDemo/BookEntityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookentities.BookEntityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEntityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
